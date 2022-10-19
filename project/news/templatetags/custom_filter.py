3c58d2cbe27a3a0bd19844308b25f28b7692b486
Alexmsr <pst.sistema@gmail.com>
2022-10-19 14:18:25 +0300

    7.2

--- project/news/templatetags/custom_filter.py
+++ project/news/templatetags/custom_filter.py
@@ -5,6 +5,7 @@ register = template.Library()
 
 # Регистрируем наш фильтр под именем currency, чтоб Django понимал,
 # что это именно фильтр для шаблонов, а не простая функция.
+
 @register.filter()
 def censor(value):
    bed = ['Редиска', 'дурак']
--- project/simpleapp/templatetags/custom_filters.py
+++ project/simpleapp/templatetags/custom_filters.py
@@ -10,6 +10,7 @@ CURRENCIES_SYMBOLS = {
 
 # Регистрируем наш фильтр под именем currency, чтоб Django понимал,
 # что это именно фильтр для шаблонов, а не простая функция.
+
 @register.filter()
 def currency(value, code='rub'):
    # value: значение, к которому нужно применить фильтр
